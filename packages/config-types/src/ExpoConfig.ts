48e07ba7eab2f4ef45a067d6e5658d5b2f609e45
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2020-11-27 13:57:33 +0100

    [config-types] feat: decouple 'android.applicationId' from 'android.package' as these are no longer the same.

--- packages/config-types/src/ExpoConfig.ts
+++ packages/config-types/src/ExpoConfig.ts
@@ -457,9 +457,13 @@ export interface Android {
    */
   publishBundlePath?: string;
   /**
-   * The package name for your Android standalone app. You make it up, but it needs to be unique on the Play Store. See [this StackOverflow question](http://stackoverflow.com/questions/6273892/android-package-name-convention).
+   * The package name for your Android standalone app. By default it is also used as the `applicationId` in your application. You can provide `applicationId` separately.
    */
   package?: string;
+  /**
+   * Unique application ID that identifies your app on the device and in Google Play Store. [Learn more](https://developer.android.com/studio/build/application-id)
+   */
+  applicationId?: string;
   /**
    * Version number required by Google Play. Increment by one for each release. Must be an integer. [Learn more](https://developer.android.com/studio/publish/versioning.html)
    */
